1384c2295f2f19c9be06624c8729779660f4730f
petemcnie <petemcnie0@gmail.com>
2020-05-18 09:24:56 +1200

    EventListener added for numbers

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 // ATTAINS ACCESS TO EACH BUTTON
 let number = document.querySelectorAll('[data-number]');
-//console.log(number)
-
+// console.log(number)
 let operator = document.querySelectorAll('[data-operator]');
-//console.log(operator)
-
+// console.log(operator)
 let decimal = document.querySelectorAll('[data-decimal]');
 // console.log(decimal)
-
 let calculate = document.querySelectorAll('[data-calculate]');
 // console.log(calculate)
-
 let clearAll = document.querySelectorAll('[data-clearAll]');
 // console.log(clearAll)
-
 let clearEntry = document.querySelectorAll('[data-clearEntry]');
 // console.log(clearEntry)
 
 
 // Setting up event listeners on each group of buttons
-number.forEach((number) => {
-    number.addEventListener('click', addNumber(), false)
-})
+for (let i = 0; i < number.length; i++) {
+    number[i].addEventListener('click', addNumber, false)
+}
 
 function addNumber () {
-    console.log()
+    
+
+
+    console.log(this.innerHTML)
 };
