bf7265161663b4199dc59339cb5675ea3bb32056
petemcnie <petemcnie0@gmail.com>
2020-05-18 12:34:39 +1200

    FirstNumber adding to array

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let number = document.querySelectorAll('[data-number]');
 // console.log(number)
 let operator = document.querySelectorAll('[data-operator]');
 // console.log(operator)
-let decimal = document.querySelectorAll('[data-decimal]');
+let decimal = document.querySelector('[data-decimal]');
 // console.log(decimal)
-let calculate = document.querySelectorAll('[data-calculate]');
+let calculate = document.querySelector('[data-calculate]');
 // console.log(calculate)
-let clearAll = document.querySelectorAll('[data-clearAll]');
+let clearAll = document.querySelector('[data-clearAll]');
 // console.log(clearAll)
-let clearEntry = document.querySelectorAll('[data-clearEntry]');
+let clearEntry = document.querySelector('[data-clearEntry]');
 // console.log(clearEntry)
 
 
@@ -18,9 +18,39 @@ for (let i = 0; i < number.length; i++) {
     number[i].addEventListener('click', addNumber, false)
 }
 
+let array2 = [];
+
 function addNumber () {
+    let num = this.innerHTML;
+    let array = [];
+    array = num;
+    array2.push(...array)
+
+  
+
+
+
+    console.log(array2)
     
 
 
-    console.log(this.innerHTML)
 };
+
+
+
+
+
+
+
+
+
+
+
+// function display (value) {
+//     let display = document.getElementById('#bottomDisplay').innerHTML;
+//     display = value;
+// }
+
+//this.innerHTML
+// console.log(firstNumber)
+// console.log(addNumber())
